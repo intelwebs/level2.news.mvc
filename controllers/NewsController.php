f79d9d831ba80c083913c6655a3fa82924a8f248
intelwebs <intelwebs@mail.ru>
2015-03-05 19:11:42 +0300

    Добавлен findByColumn

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,24 +4,32 @@ class NewsController
 {
     public function actionAll()
     {
-        $items = News::getAll();
+        $items = News::findAll();
+
         $view = new View();
         $view->items = $items;
         $view->display('news/all.php');
     }
 
+
+
     public function actionOne()
     {
-        $id = $_GET['id'];
-        $item = News::getOne($id);
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+        }
+        $item = News::findOneByPk($id);
         $view = new View();
         $view->item = $item;
         $view->display('news/one.php');
     }
 
 
+
     public function actionAdd()
     {
+        $this->getView('news/add.php');
+
         if(isset($_POST['save'])) {
             $err = false;
             if (empty($_POST['title'])) {
@@ -32,19 +40,68 @@ class NewsController
             }
 
             if ($err == false) {
-
                 $article = new News;
                 $article->title = $_POST['title'];
                 $article->content = $_POST['content'];
-                $article->insertOne();
+                //$article->insert();
 
-                header("Location: /");
-                die;
+                // Выводим содержание только что добавленной новости
+                $item = News::findOneByPk($article->insert());
+                $view = new View();
+                $view->item = $item;
+                $view->display('news/one.php');
+
             }
-
-        }else{
-            $view = new View();
-            $view->display('news/add.php');
         }
     }
+
+
+
+    public function actionUpdate($id)
+    {
+        $id = $_GET['id'];
+
+        $item = News::findOneByPk($id);
+        $view = new View();
+        $view->item = $item;
+        $view->display('news/update.php');
+
+        if(isset($_POST['save']))
+        {
+            $article = new News;
+            $article->date = $_POST['date'];
+            $article->title = $_POST['title'];
+            $article->content = $_POST['content'];
+            $article->update($id);
+            header('Location: /');
+            exit;
+        }
+    }
+
+
+
+    public function actionDel($id)
+    {
+        if(isset($_GET['id']))
+        {
+            $id = $_GET['id'];
+            $article = new News;
+            $article->delete($id);
+
+            header('Location: /');
+            exit;
+        }else{
+            echo "Нет такого id";
+        }
+    }
+
+
+
+    public function getView($path)
+    {
+        $view = new View();
+        $view->display($path);
+    }
 }
+
+
