ccfe56fdcb3ef8784202b8b660751057c9e6fcc6
intelwebs <intelwebs@mail.ru>
2015-03-05 19:30:31 +0300

    Изменены ссылки

--- views/news/all.php
+++ views/news/all.php
@@ -1,5 +1,5 @@
 <?php foreach ($items as $item): ?>
-    <a href="?act=One&id=<?php echo $item->id;?>"><?php echo $item->title; ?></a> | [<a href="?act=Update&id=<?php echo $item->id;?>">редактировать</a>] - [<a href="?act=Del&id=<?php echo $item->id;?>">удалить</a>]<br />
+    <a href="?act=One&id=<?php echo $item->id;?>"><?php echo $item->title; ?></a> | [<a href="?ctrl=Admi&act=Update&id=<?php echo $item->id;?>">редактировать</a>] - [<a href="?ctrl=Admin&act=Del&id=<?php echo $item->id;?>">удалить</a>]<br />
 <?php endforeach; ?>
 
-<p><a href="?act=Add">Добавить новость</a></p>
+<p><a href="?ctrl=Admin&act=Add">Добавить новость</a></p>
